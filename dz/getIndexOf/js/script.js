08fa6165ad8b8e3c0ae0567f57ea7b7b716f407d
Sergey Kononenko <soft333@mail.ru>
2016-07-28 13:41:09 +0300

    third element

--- dz/getIndexOf/js/script.js
+++ dz/getIndexOf/js/script.js
@@ -1,7 +1,7 @@
-var arr = [1, 5, 3, 2];
+var arr = [3, 5, 3, 2];
 
-function getIndexOf(array, element) {
-	for (var i = 0; i < array.length; i++) {
+function getIndexOf(array, element, number) {
+	for (var i = number; i < array.length; i++) {
 		if (array[i] === element){
 			return i;
 		} 
@@ -9,4 +9,4 @@ function getIndexOf(array, element) {
 	return -1; 
 }
 
-getIndexOf(arr, 3);
+getIndexOf(arr, 3,1);
